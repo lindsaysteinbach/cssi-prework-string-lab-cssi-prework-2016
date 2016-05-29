8645c0eac93550a21a476bd9789e7543187177db
lindsaysteinbach <lsteinbach@google.com>
2016-05-29 10:08:26 -0400

    Done.

--- invitation.py
+++ invitation.py
@@ -1,15 +1,29 @@
 
 ### Challenge 1 - Percy Replacement:
 def percy_replacer(string_about_percy):
-  return "Replace this with your code"
+  return string_about_percy.replace('Percy','Ron')
 
 ### Challenge 2 - String Interpolation:
 def weasley_invitation(name,day,date,month):
-  return "Replace this with your code"
+  return "The family of {name} Weasley proudly invite you to celebrate their graduation from Hogwarts on {day} the {month} of {date}. Festivities will be held at The Burrow. See you then!".format(name="Ron",day="Sunday",date="18th",month="May")
+
 ### Challenge 3 - Seating Location
 def seating_location(last_name):
-  return "Replace this with your code"
+  rear="ABCDEFG"
+  front="W"
+  
+  if (last_name[0] in rear) == True:
+      row = "the rear section"
+  elif (last_name[0] in front) == True:
+      row = "the front row"
+  else:
+      row = "the middle section"
+
+  return "You have a reserved seat in {seating_area}.".format(seating_area=row)
 
 ###Challenge 4 - All Together
 def create_invitation(first_name, last_name, message):
-  return "Replace this with your code"
+  cap_first = first_name.upper()
+  ps_message=seating_location(last_name)
+
+  return "DEAR {first},\n{msg}\nP.S. {ps_msg}".format(first=cap_first, last=last_name, msg=message, ps_msg=ps_message)
